c677cf63470ee939d1770ac347c9eeef09afd76f
efritznel <68675664+efritznel@users.noreply.github.com>
2026-01-21 17:29:46 -0500

    Fix formatting and variable naming in main.tf

--- DevOps/Terraform/main.tf
+++ DevOps/Terraform/main.tf
@@ -28,12 +28,13 @@ data "aws_ami" "amazon_linux" {
 }
 
 resource "aws_instance" "dev_machine" {
-  ami = data.aws_ami.amazon-linux.id
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
-  key_name = "euran-jenkins"
+  key_name      = "euran-jenkins"
 
   tags = {
     Environment = "dev"
-    Name = "${var.name}-server"
+    Name        = "${var.name}-server"
   }
 }
+
