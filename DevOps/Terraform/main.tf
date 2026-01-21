90e4adb72d62cdc75ee2498ca82f7dfad24f1c4e
efritznel <68675664+efritznel@users.noreply.github.com>
2026-01-21 18:14:40 -0500

    Uncomment AWS AMI data block for amazon_linux

--- DevOps/Terraform/main.tf
+++ DevOps/Terraform/main.tf
@@ -1,17 +1,3 @@
-/*data "aws_ami" "amazon-linux" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}*/
-
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["137112412989"] # Amazon
